e26efd0a2a6a8217b17902faf81cedcdcba901d6
ayush <atulg7516@gmail.com>
2021-06-07 22:33:22 +0530

    earthquakes mapped

--- 01_Mapping_Earthquakes/sketch.js
+++ 01_Mapping_Earthquakes/sketch.js
@@ -2,12 +2,17 @@ let map;
 let zoom = 1;
 let clat = 0;
 let clon = 0;
-let lat = 31.2304;
-let lon = 121.473;
+let lat = 0;
+let lon = 0;
+let earthquakes;
 
+//  PublicKeyCredential
 function preload() {
   map = loadImage(
     "https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/0,0,1,0,0/1024x512?access_token=pk.eyJ1Ijoic2lheXVzaCIsImEiOiJja3BjaW1tbm0xN25uMndxbTc2bGx1YjQ0In0.Ngp1ZLHjeBCTBJmb23fqEA"
+  );
+  earthquakes = loadStrings(
+    "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv"
   );
 }
 
@@ -34,8 +39,22 @@ function setup() {
 
   let cx = mercX(clon);
   let cy = mercY(clat);
-  let x = mercX(lon) - cx;
-  let y = mercY(lat) - cy;
-  fill(255, 0, 255, 200);
-  ellipse(x, y, 20, 20);
+
+  for (let i = 1; i < earthquakes.length; i++) {
+    let data = earthquakes[i].split(/,/);
+    console.log(data);
+    let lat = data[1];
+    let lon = data[2];
+    let mag = data[4];
+    let x = mercX(lon) - cx;
+    let y = mercY(lat) - cy;
+
+    mag = pow(10, mag);
+    mag = sqrt(mag);
+    let magmax = sqrt(pow(10, 10));
+    let d = map(mag, 0, magmax, 0, 180);
+    stroke(255, 0, 255);
+    fill(255, 0, 255, 200);
+    ellipse(x, y, d, d);
+  }
 }
