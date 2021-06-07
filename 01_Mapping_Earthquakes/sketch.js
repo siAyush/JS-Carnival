b4fcbb6a309f6b1ba032dd5f28b1dddad0f11bc3
ayush <atulg7516@gmail.com>
2021-06-07 13:37:48 +0530

    map showing

--- 01_Mapping_Earthquakes/sketch.js
+++ 01_Mapping_Earthquakes/sketch.js
@@ -1,7 +1,14 @@
+let map;
+
+function preload() {
+  map = loadImage(
+    "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/-122.4241,37.78,14.25,0,60/600x600?access_token=pk.eyJ1Ijoic2lheXVzaCIsImEiOiJja3BjaW1tbm0xN25uMndxbTc2bGx1YjQ0In0.Ngp1ZLHjeBCTBJmb23fqEA"
+  );
+}
+
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-  }
+  createCanvas(600, 600);
+  image(map, 0, 0);
+}
+
+function draw() {}
